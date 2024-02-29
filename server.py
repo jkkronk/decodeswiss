747f6768ba8fb2ef51b0a41ec78a4f0271123657
jonatank <jonatan.kronander@inivation.com>
2024-02-29 21:11:33 +0100

    kantonal dialect prompt update

--- server.py
+++ server.py
@@ -13,24 +13,10 @@ class Translation(BaseModel):
     en_translation: str = Field(..., description="Translation from swiss german to english.")
     de_translation: str = Field(..., description="Translation from swiss german to high german.")
     is_swiss_german: bool = Field(..., description="True if input text is in swiss german, else false.")
-    is_bernese_dialect: bool = Field(..., description="True if input text could be in bernese dialect, else false.")
-    is_basel_dialect: bool = Field(..., description="True if input text could be in basel dialect, else false.")
-    is_solothurn_dialect: bool = Field(..., description="True if input text could be in solothurn dialect, else false.")
-    is_aargau_dialect: bool = Field(..., description="True if input text could be in aargau dialect, else false.")
-    is_lucerne_dialect: bool = Field(..., description="True if input text could be in lucerne dialect, else false.")
-    is_zug_dialect: bool = Field(..., description="True if input text is could be zug dialect, else false.")
-    is_zurich_dialect: bool = Field(..., description="True if input text is could be zurich dialect, else false.")
-    is_stgallen_dialect: bool = Field(..., description="True if input text is could be st. gallen dialect, else false.")
-    is_thurgau_dialect: bool = Field(..., description="True if input text is could be thurgau dialect, else false.")
-    is_appenzell_dialect: bool = Field(..., description="True if input text is could be appenzell dialect, else false.")
-    is_schaffhausen_dialect: bool = Field(..., description="True if input text could be in schaffhausen dialect, else false.")
-    is_grisons_dialect: bool = Field(..., description="True if input text could be in grisons dialect, else false.")
-    is_wallis_dialect: bool = Field(..., description="True if input text could be in wallis dialect, else false.")
-    is_glarus_dialect: bool = Field(..., description="True if input text could be in glarus dialect, else false.")
-    is_uri_dialect: bool = Field(..., description="True if input text is could be uri dialect, else false.")
-    is_schwyz_dialect: bool = Field(..., description="True if input text is could be schwyz dialect, else false.")
-    is_obwalden_dialect: bool = Field(..., description="True if input text is could be obwalden or nidwalden dialect, else false.")
-
+    dialects: list[str] = Field(..., description="List of dialects that the input text could be. "
+                                      "For example for Zurich and Bern it should be in this format ['zurich', 'bern']."
+                                      "But for Basel and Solothurn it should be in this format ['basel', 'solothurn']."
+                                                 "But for Zurich only it should be in this format ['zurich'].")
 
 def translate_text(text, openai_api_key=None):
     if openai_api_key == "":
@@ -40,7 +26,11 @@ def translate_text(text, openai_api_key=None):
 
     prompt = (f"Translate this swiss german to english and german: {text}. "
               f"Indicate if the input text is in swiss german."
-              f"Indicate if the input text could be of a certain dialect of swiss german.")
+              f"Indicate what dialect the input text could be. The format is a list "
+              f"of most likely dialect or dialects. For example 'hoi zäme' it should be ['zurich']. "
+              f"Or for example for 'Zum Znüni hät's feine Schoggigipfeli gha, da chame würkli nid klage.' "
+              f"Should the answer be ['bern'] but for 'Mir göhn später in d'Stadt zum e bitzeli shoppe.' the answer"
+              f"could be ['basel', 'solothurn', 'zurich']. If you get it right you will get a bonus.")
 
     translated: Translation = client.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -75,24 +65,35 @@ def translate():
         'en_translation': result.en_translation,
         'de_translation': result.de_translation,
         'is_swiss_german': result.is_swiss_german,
-        'is_bernese_dialect': result.is_bernese_dialect,
-        'is_basel_dialect': result.is_basel_dialect,
-        'is_solothurn_dialect': result.is_solothurn_dialect,
-        'is_aargau_dialect': result.is_aargau_dialect,
-        'is_lucerne_dialect': result.is_lucerne_dialect,
-        'is_zug_dialect': result.is_zug_dialect,
-        'is_zurich_dialect': result.is_zurich_dialect,
-        'is_stgallen_dialect': result.is_stgallen_dialect,
-        'is_thurgau_dialect': result.is_thurgau_dialect,
-        'is_appenzell_dialect': result.is_appenzell_dialect,
-        'is_schaffhausen_dialect': result.is_schaffhausen_dialect,
-        'is_grisons_dialect': result.is_grisons_dialect,
-        'is_wallis_dialect': result.is_wallis_dialect,
-        'is_glarus_dialect': result.is_glarus_dialect,
-        'is_uri_dialect': result.is_uri_dialect,
-        'is_schwyz_dialect': result.is_schwyz_dialect,
-        'is_obwalden_dialect': result.is_obwalden_dialect
+        'is_bernese_dialect': list2bool(result.dialects, 'bern'),
+        'is_basel_dialect': list2bool(result.dialects, 'basel'),
+        'is_solothurn_dialect': list2bool(result.dialects, 'solothurn'),
+        'is_aargau_dialect': list2bool(result.dialects, 'aargau'),
+        'is_lucerne_dialect': list2bool(result.dialects, 'lucerne'),
+        'is_zug_dialect': list2bool(result.dialects, 'zug'),
+        'is_zurich_dialect': list2bool(result.dialects, 'zurich'),
+        'is_stgallen_dialect': list2bool(result.dialects, 'stgallen'),
+        'is_thurgau_dialect': list2bool(result.dialects, 'thurgau'),
+        'is_appenzell_dialect': list2bool(result.dialects, 'appenzell'),
+        'is_schaffhausen_dialect': list2bool(result.dialects, 'schaffhausen'),
+        'is_grisons_dialect': list2bool(result.dialects, 'grisons'),
+        'is_wallis_dialect': list2bool(result.dialects, 'wallis'),
+        'is_glarus_dialect': list2bool(result.dialects, 'glarus'),
+        'is_uri_dialect': list2bool(result.dialects, 'uri'),
+        'is_schwyz_dialect': list2bool(result.dialects, 'schwyz'),
+        'is_obwalden_dialect': list2bool(result.dialects, 'obwalden'),
     })
+
+def list2bool(dialects, city):
+    dialects = [dialect.lower() for dialect in dialects]
+    dialects = [dialect.replace('ü', 'u') for dialect in dialects]
+    dialects = [dialect.replace(' ', '') for dialect in dialects]
+    dialects = [dialect.replace('.', '') for dialect in dialects]
+    dialects = [dialect.replace('-', '') for dialect in dialects]
+    if city in dialects:
+        return True
+    return False
+
 
 
 if __name__ == '__main__':
