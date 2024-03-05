2a73d7bc021ba2bdbb65b2dbf0c8bcb7385ffe4c
jonatank <jonatan.kronander@inivation.com>
2024-03-05 15:32:09 +0100

    again raise limits

--- server.py
+++ server.py
@@ -199,7 +199,7 @@ def check_and_update_count():
     if (datetime.now().date() - last_reset).days >= 1:
         data = {'count': 0, 'last_reset': str(datetime.now().date())}
 
-    if data['count'] >= 600:
+    if data['count'] >= 6000:
         return False
 
     data['count'] += 1
